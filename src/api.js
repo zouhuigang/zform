efc3b9d2732e9a2e5c927c0225c06771e83ffe70
zouhuigang <952750120@qq.com>
2020-01-05 21:51:10 +0800

    修好

--- src/api.js
+++ src/api.js
@@ -14,7 +14,24 @@ export function fetch (url, params) {
 }
 export default {
   ERR_OK,
-  customPhpApi (url, params) {
-	return fetch(url, params)
+  customPhpApi  (url, params) {
+	let form_data = {}
+	let data = {}
+	let type = ''
+	for (var i = 0; i < params.length; i++) {
+		if(ele.name== 'cscode'){
+			type = ele.value
+		}else{
+			let ele = params[i];
+			data[ele.name] = ele.value
+		}
+		
+	
+	}
+	
+    form_data["type"]=type
+    form_data["data"]=JSON.stringify(data)
+	console.info(form_data);
+	return fetch(url, form_data)
   },
 }
