bc08c1f04091087862850613102b24eaceb8a8b8
zouhuigang <952750120@qq.com>
2020-01-05 14:49:50 +0800

    通过script引入然后开发

--- src/zform.js
+++ src/zform.js
@@ -1,14 +1,33 @@
 const axios = require('axios');
-
-function showString ( str ) {
-	return str;
-}
+const zjq = require('jquery');
+let util = require('./util.js');
 
 const zform = {
+	id:'formid',
 	versionNo: '1.0.0',
 	version () {
-		return showString(this.versionNo)
+		return util.showString(this.versionNo)
 	}
 }
+
+zform.config = ({form_id, api_url }) => {
+	this.id = form_id
+	return api_url;
+}
+
+zform.println = () => {
+	let eleList = util.getElements(this.id);
+	console.info(eleList);
+	const mobile = zjq('input[name="mobile"]').val();
+	alert(mobile);
+}
+
+
+
+// exports default {
+//     add: function (num1, num2) {
+//          return num1 + num2; 
+//     }         
+// };
 
 module.exports = zform
